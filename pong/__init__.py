9cade43426641072f50246f985248a74f0297e86
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2020-06-26 23:09:34 -0300

    :bug: Conserta o registro de ambientes

--- pong/__init__.py
+++ pong/__init__.py
@@ -4,13 +4,13 @@ from gym.envs.registration import register
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 register(
-    id='turing-easy-v0',
+    id='turing-normal-v0',
     entry_point='pong.objects:PongEnv',
     max_episode_steps=10_000,
 )
 
 register(
-    id='turing-normal-v0',
+    id='turing-easy-v0',
     entry_point='pong.objects:EasyPongEnv',
     max_episode_steps=10_000,
 )
